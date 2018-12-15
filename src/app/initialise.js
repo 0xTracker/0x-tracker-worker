5048803809cc569c1b0183dbffe9f9009eec1bce
Craig Bovis <craig.bovis@inlight.com.au>
2018-12-15 20:43:26 +1100

    Disable ercdex recipient cache polling

--- src/app/initialise.js
+++ src/app/initialise.js
@@ -1,7 +1,7 @@
-const config = require('config');
+// const config = require('config');
 
 const configure = require('./configure');
-const ercDexRecipientCache = require('../relayers/erc-dex/recipient-cache');
+// const ercDexRecipientCache = require('../relayers/erc-dex/recipient-cache');
 const populateRelayersCollection = require('../relayers/populate-relayers-collection');
 const tokenCache = require('../tokens/token-cache');
 
@@ -9,11 +9,11 @@ const initialise = async () => {
   configure();
 
   await Promise.all([
-    ercDexRecipientCache.loadRecipients().then(() => {
-      ercDexRecipientCache.startPolling(
-        config.get('ercDex.feeRecipientPollingInterval'),
-      );
-    }),
+    // ercDexRecipientCache.loadRecipients().then(() => {
+    //   ercDexRecipientCache.startPolling(
+    //     config.get('ercDex.feeRecipientPollingInterval'),
+    //   );
+    // }),
     tokenCache.initialise(),
     populateRelayersCollection(),
   ]);
