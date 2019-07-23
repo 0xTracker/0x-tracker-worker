acbf14a11215cc7332d82c05fca2c33bb3dadbe8
Craig Bovis <craig@bovis.me.uk>
2019-07-23 14:40:43 +0700

    Remove noisy logging

--- src/jobs/create-fills/index.js
+++ src/jobs/create-fills/index.js
@@ -45,9 +45,7 @@ const createFills = async ({ batchSize }) => {
           `Unable to create fill for event ${event.id} due to missing block`,
         );
       } else if (error instanceof UnsupportedAssetError) {
-        logger.warn(
-          `Unable to create fill for event ${event.id} due to unsupported asset`,
-        );
+        // ignore
       } else if (error instanceof UnsupportedProtocolError) {
         logger.warn(
           `Unable to create fill for event ${event.id} due to unsupported protocol`,
