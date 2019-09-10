03f72eb2ef875edc6935064eeca51716ab9b9733
Craig Bovis <craig@bovis.me.uk>
2019-09-10 20:03:19 +0200

    Split maker/taker metrics per address (#296)

--- src/model/address-metric.js
+++ src/model/address-metric.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
+const { Schema } = mongoose;
+
 const metricShape = {
   date: Date,
-  fillCount: Number,
-  fillVolume: Number,
+  fillCount: Schema.Types.Mixed,
+  fillVolume: Schema.Types.Mixed,
 };
 
 const schema = mongoose.Schema({
