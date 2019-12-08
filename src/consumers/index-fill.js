3f8cb842200d5c2a4ccef7496c6bcd6b17816324
Craig Bovis <craig@bovis.me.uk>
2019-12-08 19:43:04 +0000

    Wireup reindexing consumer (#359)
    * Wireup consumer and improve logging
    
    * Add guard to reindexing consumer
    
    * Improve index-fill consumer error handling
    
    * Improved fetch-fill-status consumer error handling

--- src/consumers/index-fill.js
+++ src/consumers/index-fill.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const signale = require('signale');
 
 const { getModel } = require('../model');
@@ -6,9 +7,18 @@ const logger = signale.scope('index fill');
 
 const indexFillConsumer = async (job, done) => {
   const { fillId } = job.data;
-  const fill = await getModel('Fill').findOne({ _id: fillId });
+
+  if (!mongoose.Types.ObjectId.isValid(fillId)) {
+    throw new Error(`Invalid fillId: ${fillId}`);
+  }
 
   logger.info(`indexing fill: ${fillId}`);
+
+  const fill = await getModel('Fill').findOne({ _id: fillId });
+
+  if (fill === null) {
+    throw new Error(`No fill found with the id: ${fillId}`);
+  }
 
   fill.index(error => {
     if (error) {
