c311caa589ffa3cfd62b9e6a6cf91765e98ac866
Craig Bovis <craig@bovis.me.uk>
2019-12-19 08:29:24 +0000

    Remove redundant transaction

--- src/consumers/fetch-fill-status.js
+++ src/consumers/fetch-fill-status.js
@@ -7,7 +7,6 @@ const { FILL_STATUS, JOB, QUEUE } = require('../constants');
 const { getModel } = require('../model');
 const { publishJob } = require('../queues');
 const getTransactionReceipt = require('../util/ethereum/get-transaction-receipt');
-const withTransaction = require('../util/with-transaction');
 
 const logger = signale.scope('fetch fill status');
 
@@ -32,20 +31,18 @@ const fetchFillStatus = async job => {
     receipt.status === 0 ? FILL_STATUS.FAILED : FILL_STATUS.SUCCESSFUL;
   const statusText = _.findKey(FILL_STATUS, value => value === status);
 
-  await withTransaction(async session => {
-    await getModel('Fill').updateOne({ _id: fillId }, { status }, { session });
-    await publishJob(
-      QUEUE.FILL_INDEXING,
-      JOB.INDEX_FILL,
-      {
-        fillId,
-      },
-      {
-        delay: ms('30 seconds'),
-        removeOnComplete: true,
-      },
-    );
-  });
+  await getModel('Fill').updateOne({ _id: fillId }, { status });
+  await publishJob(
+    QUEUE.FILL_INDEXING,
+    JOB.INDEX_FILL,
+    {
+      fillId,
+    },
+    {
+      delay: ms('30 seconds'),
+      removeOnComplete: true,
+    },
+  );
 
   logger.success(`set status of fill ${fillId} to ${statusText}`);
 };
