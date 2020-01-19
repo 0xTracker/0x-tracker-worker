46e365b4445a494add4e43cd6b88fc134ad50e28
Craig Bovis <craig@bovis.me.uk>
2020-01-19 17:31:42 +0000

    Add a 10s timeout when fetching transaction receipts (#380)

--- src/util/ethereum/get-transaction-receipt.js
+++ src/util/ethereum/get-transaction-receipt.js
@@ -2,9 +2,22 @@ const web3 = require('./web3');
 
 const getTransactionReceipt = async transactionHash => {
   const web3Wrapper = web3.getWrapper();
-  const receipt = await web3Wrapper.getTransactionReceiptIfExistsAsync(
-    transactionHash,
-  );
+
+  const timeout = new Promise((resolve, reject) => {
+    const id = setTimeout(() => {
+      clearTimeout(id);
+      reject(
+        new Error(
+          `Fetching transaction receipt ${transactionHash} timed out after 10s`,
+        ),
+      );
+    }, 10000);
+  });
+
+  const receipt = await Promise.race([
+    web3Wrapper.getTransactionReceiptIfExistsAsync(transactionHash),
+    timeout,
+  ]);
 
   return receipt;
 };
