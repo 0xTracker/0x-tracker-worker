11d3a4a7ea6fcb9509160d749f25c181652524be
Craig Bovis <craig@bovis.me.uk>
2020-01-20 19:12:21 +0000

    Add multi-collateral DAI to base tokens

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ module.exports = {
     '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359': 'DAI', // DAI Stablecoin
     '0xd9ebebfdab08c643c5f2837632de920c70a56247': 'DAI', // Ethfinex DAI
     '0x2da4f4ff3eb51bff53b66f00054d6cf8d028349f': 'DAI', // Ethfinex DAI V2
+    '0x6b175474e89094c44da98b954eedeac495271d0f': 'DAI', // Multi-collateral DAI
     '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 'USDC', // USD Coin
     '0xdac17f958d2ee523a2206206994597c13d831ec7': 'USDT', // Tether
     '0x1a9b2d827f26b7d7c18fec4c1b27c1e8deeba26e': 'USDT', // Ethfinex Tether
