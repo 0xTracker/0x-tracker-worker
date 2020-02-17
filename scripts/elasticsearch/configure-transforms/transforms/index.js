70bdaab80a3a88bf25be650c9914bb0b6edbc194
Craig Bovis <craig@bovis.me.uk>
2020-02-17 19:29:06 +0000

    Add maker/taker metrics transforms

--- scripts/elasticsearch/configure-transforms/transforms/index.js
+++ scripts/elasticsearch/configure-transforms/transforms/index.js
@@ -2,9 +2,11 @@ const activeTraderMetricsHourly = require('./active-trader-metrics-hourly');
 const activeTraderMetricsDaily = require('./active-trader-metrics-daily');
 const activeTraderMetricsWeekly = require('./active-trader-metrics-weekly');
 const activeTraderMetricsMonthly = require('./active-trader-metrics-monthly');
+const makerMetricsHourly = require('./maker-metrics-hourly');
 const networkMetricsHourly = require('./network-metrics-hourly');
 const protocolMetricsHourly = require('./protocol-metrics-hourly');
 const relayerMetricsHourly = require('./relayer-metrics-hourly');
+const takerMetricsHourly = require('./taker-metrics-hourly');
 const traderMetricsHourly = require('./trader-metrics-hourly');
 const unknownRelayerMetricsHourly = require('./unknown-relayer-metrics-hourly');
 
@@ -13,9 +15,11 @@ module.exports = [
   activeTraderMetricsHourly,
   activeTraderMetricsMonthly,
   activeTraderMetricsWeekly,
+  makerMetricsHourly,
   networkMetricsHourly,
   protocolMetricsHourly,
   relayerMetricsHourly,
+  takerMetricsHourly,
   traderMetricsHourly,
   unknownRelayerMetricsHourly,
 ];
---
+++ scripts/elasticsearch/configure-transforms/transforms/maker-metrics-hourly.js
@@ -0,0 +1,56 @@
+module.exports = {
+  name: 'maker_metrics_hourly',
+  config: {
+    description: 'Hourly maker metrics',
+    dest: {
+      index: 'maker_metrics_hourly',
+    },
+    sync: {
+      time: {
+        delay: '60s',
+        field: 'updatedAt',
+      },
+    },
+    source: {
+      index: ['fills'],
+    },
+    frequency: '1m',
+    pivot: {
+      group_by: {
+        date: {
+          date_histogram: {
+            field: 'date',
+            calendar_interval: '1h',
+          },
+        },
+        maker: {
+          terms: {
+            field: 'maker.keyword',
+          },
+        },
+      },
+      aggregations: {
+        fillCount: {
+          value_count: {
+            field: '_id',
+          },
+        },
+        fillVolume: {
+          sum: {
+            field: 'value',
+          },
+        },
+        tradeCount: {
+          sum: {
+            field: 'tradeCountContribution',
+          },
+        },
+        tradeVolume: {
+          sum: {
+            field: 'tradeVolume',
+          },
+        },
+      },
+    },
+  },
+};
