ebab6e7cf6bfc771dacda76345c72d81378b1041
Craig Bovis <craig@bovis.me.uk>
2020-03-29 13:15:17 -0500

    Add tests for consumer

---
+++ src/consumers/convert-protocol-fee/index.test.js
@@ -0,0 +1,83 @@
+const { fn } = require('./');
+const getConversionRate = require('../../rates/get-conversion-rate');
+const persistConvertedProtocolFee = require('./persist-converted-protocol-fee');
+const withTransaction = require('../../util/with-transaction');
+
+jest.mock('./persist-converted-protocol-fee');
+jest.mock('../../rates/get-conversion-rate');
+jest.mock('../../queues');
+jest.mock('../../util/with-transaction');
+
+const stubSession = {};
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  withTransaction.mockImplementation(async wrappedFn => {
+    await wrappedFn(stubSession);
+  });
+});
+
+describe('convert protocol fee consumer', () => {
+  it('should throw an error when fillId is invalid', async () => {
+    await expect(
+      fn({
+        fillDate: '2020-03-29T19:30:00.000Z',
+        fillId: 'fubar',
+        protocolFee: 1000000000000000,
+      }),
+    ).rejects.toThrow(new Error('Invalid fillId: fubar'));
+  });
+
+  it('should throw an error when fillDate is invalid', async () => {
+    await expect(
+      fn({
+        fillDate: 'fubar',
+        fillId: '5dab535fa75e77be63cfcc29',
+        protocolFee: 1000000000000000,
+      }),
+    ).rejects.toThrow(new Error('Invalid fillDate: fubar'));
+  });
+
+  it('should throw an error when protocolFee is invalid', async () => {
+    await expect(
+      fn({
+        fillDate: '2020-03-29T19:30:00.000Z',
+        fillId: '5dab535fa75e77be63cfcc29',
+        protocolFee: 'fubar',
+      }),
+    ).rejects.toThrow(new Error('Invalid protocolFee: fubar'));
+  });
+
+  it('should persist converted fee when conversion rate available', async () => {
+    getConversionRate.mockResolvedValue(180);
+
+    await fn({
+      fillDate: '2020-03-29T19:30:00.000Z',
+      fillId: '5dab535fa75e77be63cfcc29',
+      protocolFee: 1000000000000000,
+    });
+
+    expect(persistConvertedProtocolFee).toHaveBeenCalledTimes(1);
+    expect(persistConvertedProtocolFee).toHaveBeenCalledWith(
+      '5dab535fa75e77be63cfcc29',
+      0.18,
+      stubSession,
+    );
+  });
+
+  it('should throw error when conversion rate cannot be fetched', async () => {
+    getConversionRate.mockResolvedValueOnce(undefined);
+
+    await expect(
+      fn({
+        fillDate: '2020-03-29T19:30:00.000Z',
+        fillId: '5dab535fa75e77be63cfcc29',
+        protocolFee: 1000000000000000,
+      }),
+    ).rejects.toThrow(
+      new Error(
+        'Unable to fetch ETH conversion rate for 2020-03-29T19:30:00.000Z',
+      ),
+    );
+  });
+});
