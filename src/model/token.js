3f99b204fefd912d0aaf40aa649899b4a73b42fa
Craig Bovis <craig@bovis.me.uk>
2020-03-29 18:26:32 -0500

    Add timestamps to tokens

--- src/model/token.js
+++ src/model/token.js
@@ -2,22 +2,25 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const schema = Schema({
-  address: String,
-  decimals: Number,
-  imageUrl: { type: String, trim: true },
-  name: String,
-  price: {
-    lastTrade: {
-      date: Date,
-      id: { type: Schema.Types.ObjectId, Ref: 'Fill' },
+const schema = Schema(
+  {
+    address: String,
+    decimals: Number,
+    imageUrl: { type: String, trim: true },
+    name: String,
+    price: {
+      lastTrade: {
+        date: Date,
+        id: { type: Schema.Types.ObjectId, Ref: 'Fill' },
+      },
+      lastPrice: Number,
     },
-    lastPrice: Number,
+    resolved: Boolean,
+    symbol: String,
+    type: Number,
   },
-  resolved: Boolean,
-  symbol: String,
-  type: Number,
-});
+  { timestamps: true },
+);
 
 schema.index({ address: 1 }, { unique: true });
 schema.index({ resolved: 1 });
