ce91fdfd996ff385f714a1227ea92fdf2bb109e8
Craig Bovis <craig@bovis.me.uk>
2020-03-29 14:27:41 -0500

    Handle persistance failure gracefully

--- src/consumers/convert-protocol-fee/persist-converted-protocol-fee.js
+++ src/consumers/convert-protocol-fee/persist-converted-protocol-fee.js
@@ -5,11 +5,17 @@ const persistConvertedProtocolFee = async (
   convertedProtocolFee,
   dbSession,
 ) => {
-  await getModel('Fill').updateOne(
+  const result = await getModel('Fill').updateOne(
     { _id: fillId },
     { $set: { 'conversions.USD.protocolFee': convertedProtocolFee } },
     { session: dbSession },
   );
+
+  if (result.nModified !== 1) {
+    throw new Error(
+      `Could not persist converted protocol fee of fill: ${fillId}`,
+    );
+  }
 };
 
 module.exports = persistConvertedProtocolFee;
