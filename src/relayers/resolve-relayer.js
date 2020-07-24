54e072401cde303ee67cfac405b9e3971b17f374
Brandon Curtis <admin@brandoncurtis.net>
2020-07-24 11:40:39 -0700

    Attribute RADAR RELAY volume
    RADAR operates an instance of 0xAPI that powers RADAR RELAY at https://relay.radar.tech.
    This 0xAPI instance uses the RADAR RELAY `feeAddress` as the `affiliateAddress`.
    Properly attribute this volume using the `affiliateAddress`.

--- src/relayers/resolve-relayer.js
+++ src/relayers/resolve-relayer.js
@@ -22,11 +22,21 @@ const resolveRelayer = metadata => {
   // TODO: Remove this temporary hack once apps feature is in place
   if (
     (affiliateAddress === '0x86003b044f70dac0abc80ac8957305b6370893ed' && // Matcha
-      feeRecipient === '0x1000000000000000000000000000000000000011') || // 0x API
+      feeRecipient === '0x1000000000000000000000000000000000000011') || // 0x Team 0xAPI
     (affiliateAddress === '0x86003b044f70dac0abc80ac8957305b6370893ed' && // Matcha
       matchingRelayer === undefined)
   ) {
     return relayers.matcha;
+  }
+  
+  // TODO: Remove this temporary hack once apps feature is in place
+  if (
+    (affiliateAddress === '0xa258b39954cef5cb142fd567a46cddb31a670124' && // RADAR RELAY
+      feeRecipient === '0x0000000000000000000000000000000000000000') || // RADAR 0xAPI
+    (affiliateAddress === '0xa258b39954cef5cb142fd567a46cddb31a670124' && // RADAR RELAY
+      matchingRelayer === undefined)
+  ) {
+    return relayers.radarRelay;
   }
 
   return matchingRelayer || null;
