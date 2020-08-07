172c0c4be140824e3b3f21af50af8c9aa1feebff
Craig Bovis <craig@bovis.me.uk>
2020-08-07 19:49:38 +0200

    Add logging to debug issue

--- src/rates/get-conversion-rate.js
+++ src/rates/get-conversion-rate.js
@@ -1,11 +1,22 @@
 const _ = require('lodash');
 
+const { logError } = require('../util/error-logger');
 const getRates = require('./get-rates');
 
 const getConversionRate = async (fromSymbol, toSymbol, date) => {
   const rates = await getRates(fromSymbol, date);
+  const conversionRate = _.get(rates, [fromSymbol, toSymbol]);
 
-  return _.get(rates, [fromSymbol, toSymbol]);
+  if (conversionRate === undefined) {
+    logError(
+      `Unable to fetch conversion rate from ${fromSymbol} to ${toSymbol} on ${date}`,
+      {
+        rates,
+      },
+    );
+  }
+
+  return conversionRate;
 };
 
 module.exports = getConversionRate;
