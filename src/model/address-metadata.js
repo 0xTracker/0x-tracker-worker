8ccf32ba1c853e764fb14db460ab043b7e5f8424
Craig Bovis <craig@bovis.me.uk>
2020-09-13 14:46:17 +0200

    Update AddressMetadata model

--- src/model/address-metadata.js
+++ src/model/address-metadata.js
@@ -4,9 +4,11 @@ const { Schema } = mongoose;
 
 const schema = Schema({
   address: String,
+  description: String,
   imageUrl: String,
   isContract: Boolean,
   name: String,
+  url: String,
 });
 
 const AddressMetadata = mongoose.model('AddressMetadata', schema);
