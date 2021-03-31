1e27edaeccc0c40467ab6eb5c493e1fddf0ccc9c
Craig Bovis <craig@bovis.me.uk>
2021-03-30 22:21:41 -0500

    Update app_fills index after measurement

--- src/index/index-app-fills.js
+++ src/index/index-app-fills.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const { JOB, QUEUE } = require('../constants');
+const { JOB, QUEUE, FILL_ATTRIBUTION_TYPE } = require('../constants');
 const { publishJob } = require('../queues');
 const relayerRegistry = require('../relayers/relayer-registry');
 
@@ -20,13 +20,26 @@ const calculateTradeCount = relayerId => {
 };
 
 const indexAppFills = async fill => {
+  if (
+    !fill.attributions.some(
+      a =>
+        a.type === FILL_ATTRIBUTION_TYPE.CONSUMER ||
+        a.type === FILL_ATTRIBUTION_TYPE.RELAYER,
+    )
+  ) {
+    return;
+  }
+
   const fillId = fill._id.toString();
   const value = _.get(fill, 'conversions.USD.amount');
   const tradeCount = calculateTradeCount(fill.relayerId);
   const tradeValue = value ? value * tradeCount : null;
 
   publishJob(QUEUE.INDEXING, JOB.INDEX_APP_FILLS, {
-    attributions: fill.attributions,
+    attributions: fill.attributions.map(a => ({
+      entityId: a.entityId,
+      type: a.type,
+    })),
     fillDate: fill.date,
     fillId,
     maker: fill.maker,
