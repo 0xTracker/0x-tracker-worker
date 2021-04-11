fddf009562669083a9b6e63554574308b111b865
Craig Bovis <craig@bovis.me.uk>
2021-04-11 11:50:01 -0500

    Calculate change in basic stats

--- src/jobs/precompute-app-stats.js
+++ src/jobs/precompute-app-stats.js
@@ -7,6 +7,7 @@ const getPercentageChange = require('../util/get-percentage-change');
 const getPreviousPeriod = require('../util/get-previous-period');
 
 const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
+  const start = Date.now();
   const dates = getDatesForTimePeriod(periodInDays);
   const apps = await AttributionEntity.find();
 
@@ -21,7 +22,7 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
         apps: {
           terms: {
             field: 'appId',
-            size: 100,
+            size: 100, // In future this may need to increased or be paginated
           },
           aggs: {
             relayedTrades: {
@@ -63,46 +64,12 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
 
   const appIds = basicData.body.aggregations.apps.buckets.map(b => b.key);
 
-  const [activeTradersData, previousActiveTradersData] = await Promise.all([
-    elasticsearch.getClient().search({
-      index: 'trader_metrics_daily',
-      size: 0,
-      body: {
-        aggs: {
-          apps: {
-            terms: {
-              field: 'appIds',
-              size: 100,
-            },
-            aggs: {
-              activeTraders: {
-                cardinality: {
-                  field: 'address',
-                },
-              },
-            },
-          },
-        },
-        query: {
-          bool: {
-            filter: _.compact([
-              dates === null
-                ? null
-                : {
-                    range: {
-                      date: {
-                        gte: dates.dateFrom,
-                        lte: dates.dateTo,
-                      },
-                    },
-                  },
-              { terms: { appIds } },
-            ]),
-          },
-        },
-      },
-    }),
-    periodInDays === null
+  const [
+    activeTradersData,
+    previousActiveTradersData,
+    previousStatsData,
+  ] = await Promise.all([
+    appIds.length === 0
       ? null
       : elasticsearch.getClient().search({
           index: 'trader_metrics_daily',
@@ -112,7 +79,7 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
               apps: {
                 terms: {
                   field: 'appIds',
-                  size: 100,
+                  size: appIds.length,
                 },
                 aggs: {
                   activeTraders: {
@@ -124,11 +91,111 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
               },
             },
             query: {
-              range: {
-                date: {
-                  gte: previousPeriod.prevDateFrom,
-                  lte: previousPeriod.prevDateTo,
+              bool: {
+                filter: _.compact([
+                  dates === null
+                    ? null
+                    : {
+                        range: {
+                          date: {
+                            gte: dates.dateFrom,
+                            lte: dates.dateTo,
+                          },
+                        },
+                      },
+                  { terms: { appIds } },
+                ]),
+              },
+            },
+          },
+        }),
+    periodInDays === null || appIds.length === 0
+      ? null
+      : elasticsearch.getClient().search({
+          index: 'trader_metrics_daily',
+          size: 0,
+          body: {
+            aggs: {
+              apps: {
+                terms: {
+                  field: 'appIds',
+                  size: appIds.length,
                 },
+                aggs: {
+                  activeTraders: {
+                    cardinality: {
+                      field: 'address',
+                    },
+                  },
+                },
+              },
+            },
+            query: {
+              bool: {
+                filter: [
+                  {
+                    range: {
+                      date: {
+                        gte: previousPeriod.prevDateFrom,
+                        lte: previousPeriod.prevDateTo,
+                      },
+                    },
+                  },
+                  { terms: { appIds } },
+                ],
+              },
+            },
+          },
+        }),
+    periodInDays === null || appIds.length === 0
+      ? null
+      : elasticsearch.getClient().search({
+          index: 'app_metrics_daily',
+          size: 0,
+          body: {
+            aggs: {
+              apps: {
+                terms: {
+                  field: 'appId',
+                  size: appIds.length,
+                },
+                aggs: {
+                  relayedTrades: {
+                    sum: {
+                      field: 'relayedTradeCount',
+                    },
+                  },
+                  relayedVolume: {
+                    sum: {
+                      field: 'relayedTradeValue',
+                    },
+                  },
+                  totalTrades: {
+                    sum: {
+                      field: 'totalTradeCount',
+                    },
+                  },
+                  totalVolume: {
+                    sum: {
+                      field: 'totalTradeValue',
+                    },
+                  },
+                },
+              },
+            },
+            query: {
+              bool: {
+                filter: [
+                  {
+                    range: {
+                      date: {
+                        gte: previousPeriod.prevDateFrom,
+                        lte: previousPeriod.prevDateTo,
+                      },
+                    },
+                  },
+                  { terms: { appId: appIds } },
+                ],
               },
             },
           },
@@ -148,6 +215,12 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
       [],
     ).find(b => b.key === app.id);
 
+    const previousStats = _.get(
+      previousStatsData,
+      'body.aggregations.apps.buckets',
+      [],
+    ).find(b => b.key === app.id);
+
     return {
       appId: bucket.key,
       activeTraders: _.get(activeTraders, 'activeTraders.value', 0),
@@ -156,17 +229,33 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
         _.get(activeTraders, 'activeTraders.value', 0),
       ),
       appName: app.name,
-      periodInDays,
+      period: periodInDays === null ? 'all-time' : `${periodInDays}d`,
       relayedTrades: bucket.relayedTrades.value,
+      relayedTradesChange: getPercentageChange(
+        _.get(previousStats, 'relayedTrades.value', 0),
+        _.get(bucket, 'relayedTrades.value', 0),
+      ),
       relayedVolume: bucket.relayedVolume.value,
+      relayedVolumeChange: getPercentageChange(
+        _.get(previousStats, 'relayedVolume.value', 0),
+        _.get(bucket, 'relayedVolume.value', 0),
+      ),
       totalTrades: bucket.totalTrades.value,
+      totalTradesChange: getPercentageChange(
+        _.get(previousStats, 'totalTrades.value', 0),
+        _.get(bucket, 'totalTrades.value', 0),
+      ),
       totalVolume: bucket.totalVolume.value,
+      totalVolumeChange: getPercentageChange(
+        _.get(previousStats, 'totalVolume.value', 0),
+        _.get(bucket, 'totalVolume.value', 0),
+      ),
     };
   });
 
   const operations = appStats.map(document => ({
     updateOne: {
-      filter: { appId: document.appId, periodInDays: document.periodInDays },
+      filter: { appId: document.appId, period: document.period },
       update: {
         $set: document,
       },
@@ -179,8 +268,11 @@ const precomputeAppStatsForPeriod = async (periodInDays, { logger }) => {
 
   if (operations.length > 0) {
     await AppStats.bulkWrite(operations);
+
+    const timeTaken = Date.now() - start;
+
     logger.info(
-      `precomputed ${periodDescriptor} stats for ${appStats.length} app(s)`,
+      `precomputed ${periodDescriptor} stats for ${appStats.length} app(s) in ${timeTaken}ms`,
     );
   } else {
     logger.warn(`no ${periodDescriptor} app trading activity found`);
--- src/model/app-stats.js
+++ src/model/app-stats.js
@@ -7,11 +7,15 @@ const schema = Schema({
   activeTradersChange: Number,
   appId: { required: true, type: String },
   appName: { required: true, type: String },
-  periodInDays: Number,
+  period: String,
   relayedTrades: { required: true, type: Number },
+  relayedTradesChange: Number,
   relayedVolume: { required: true, type: Number },
+  relayedVolumeChange: Number,
   totalTrades: { required: true, type: Number },
+  totalTradesChange: Number,
   totalVolume: { required: true, type: Number },
+  totalVolumeChange: Number,
 });
 
 const AppStats = mongoose.model('AppStats', schema);
