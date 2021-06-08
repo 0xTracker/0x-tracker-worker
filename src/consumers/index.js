0dfd8875b599dfa14ede87cbf029c5577bbf11ac
Jorge Pérez <jorge@0xproject.com>
2021-06-08 15:15:04 -0500

    Remove redundant consumer for Uniswap v3 VIP

--- src/consumers/index.js
+++ src/consumers/index.js
@@ -11,7 +11,6 @@ const createSushiswapSwapEventFill = require('./create-sushiswap-swap-event-fill
 const createToken = require('./create-token');
 const createTransformedERC20EventFills = require('./create-transformed-erc20-event-fills');
 const createUniswapV2SwapEventFill = require('./create-uniswap-v2-swap-event-fill');
-const createUniswapV3SwapEventFill = require('./create-uniswap-v3-swap-event-fill');
 const fetchAddressType = require('./fetch-address-type');
 const fetchFillStatus = require('./fetch-fill-status');
 const fetchTokenMetadata = require('./fetch-token-metadata');
@@ -33,7 +32,6 @@ const consumers = [
   createToken,
   createTransformedERC20EventFills,
   createUniswapV2SwapEventFill,
-  createUniswapV3SwapEventFill,
   fetchAddressType,
   fetchFillStatus,
   fetchTokenMetadata,
