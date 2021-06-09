f0e3818af01868c1488c6b29b8abdd5f4506765f
Craig Bovis <craig@bovis.me.uk>
2021-06-09 21:00:47 +0200

    Remove redundant jobs

--- src/consumers/index.js
+++ src/consumers/index.js
@@ -6,11 +6,7 @@ const bulkUpdateTokenMetadata = require('./bulk-update-token-metadata');
 const convertProtocolFee = require('./convert-protocol-fee');
 const convertRelayerFees = require('./convert-relayer-fees');
 const createFillsForEvent = require('./create-fills-for-event');
-const createLiquidityProviderSwapEventFill = require('./create-liquidity-provider-swap-event-fill');
-const createSushiswapSwapEventFill = require('./create-sushiswap-swap-event-fill');
 const createToken = require('./create-token');
-const createTransformedERC20EventFills = require('./create-transformed-erc20-event-fills');
-const createUniswapV2SwapEventFill = require('./create-uniswap-v2-swap-event-fill');
 const fetchAddressType = require('./fetch-address-type');
 const fetchFillStatus = require('./fetch-fill-status');
 const fetchTokenMetadata = require('./fetch-token-metadata');
@@ -27,11 +23,7 @@ const consumers = [
   convertProtocolFee,
   convertRelayerFees,
   createFillsForEvent,
-  createLiquidityProviderSwapEventFill,
-  createSushiswapSwapEventFill,
   createToken,
-  createTransformedERC20EventFills,
-  createUniswapV2SwapEventFill,
   fetchAddressType,
   fetchFillStatus,
   fetchTokenMetadata,
