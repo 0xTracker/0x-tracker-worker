a9314f5ec04140cca162282a86daa3397e6ca03c
Craig Bovis <craig@bovis.me.uk>
2021-06-12 10:27:37 +0200

    Fix edge-case in transaction fetch

--- src/consumers/fetch-transaction/build-transaction.js
+++ src/consumers/fetch-transaction/build-transaction.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const parseTransactionData = require('../../transactions/parse-transaction-data');
 
 const buildTransaction = (rawTx, txReceipt, block) => {
@@ -9,16 +10,16 @@ const buildTransaction = (rawTx, txReceipt, block) => {
     blockNumber: rawTx.blockNumber,
     data: rawTx.data,
     date: new Date(block.timestamp * 1000),
-    from: rawTx.from.toLowerCase(),
-    gasLimit: rawTx.gasLimit.toNumber(),
-    gasPrice: rawTx.gasPrice.toString(),
-    gasUsed: txReceipt.gasUsed.toNumber(),
+    from: _.lowerCase(rawTx.from),
+    gasLimit: _.toNumber(rawTx.gasLimit),
+    gasPrice: _.toString(rawTx.gasPrice),
+    gasUsed: _.toNumber(txReceipt.gasUsed),
     hash: rawTx.hash,
     index: txReceipt.transactionIndex,
     nonce: rawTx.nonce,
     quoteDate,
-    to: rawTx.to.toLowerCase(),
-    value: rawTx.value.toString(),
+    to: _.lowerCase(rawTx.to),
+    value: _.toString(rawTx.value),
   };
 };
 
