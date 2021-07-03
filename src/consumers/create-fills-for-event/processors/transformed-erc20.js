8b081edaab149daba557418fc867e3178df7e342
Craig Bovis <craig@bovis.me.uk>
2021-07-03 19:39:37 +0200

    Remove additional hack

--- src/consumers/create-fills-for-event/processors/transformed-erc20.js
+++ src/consumers/create-fills-for-event/processors/transformed-erc20.js
@@ -15,69 +15,8 @@ const Event = require('../../../model/event');
 const Fill = require('../../../model/fill');
 const withTransaction = require('../../../util/with-transaction');
 
-const SOURCE_BRIDGE_MAPPINGS = {
-  '0': '0xfe01821ca163844203220cd08e4f2b2fb43ae4e4', // Balancer
-  '1': '0xc880c252db7c51f74161633338a3bdafa8e65276', // Bancor
-  '3': '0x1796cd592d19e3bcd744fbb025bb61a6d8cb2c09', // Curve
-  '4': '0xb9d4bf2c8dab828f4ffb656acdb6c2b497d44f25', // Cream
-  '5': '0x015850307f6aab4ac6631923ceefe71b57492c9b', // Crypto.com
-  '6': '0xe9da66965a9344aab2167e6813c03f043cc7a6ca', // DODO
-  '7': '0xadd97271402590564ddd8ad23cb5317b1fb0fffb', // Kyber
-  '9': '0x02b7eca484ad960fca3f7709e0b2ac81eec3069c', // Mooniswap
-  '10': '0x2bf04fcea05f0989a14d9afa37aa376baca6b2b3', // mStable
-  '11': '0x991c745401d5b5e469b8c3e2cb02c748f08754f1', // Oasis
-  '12': '0xf1c0811e3788caae7dbfae43da9d9131b1a8a148', // Shell
-  '14': '0x47ed0262a0b688dcb836d254c6a2e96b6c48a9f5', // Sushiswap
-  '15': '0xf9786d5eb1de47fa56a8f7bb387653c6d410bfee', // Swerve
-  '16': '0x36691c4f426eb8f42f150ebde43069a31cb080ad', // Uniswap v1
-  '17': '0xdcd6011f4c6b80e470d9487f5871a0cba7c93f48', // Uniswap v2
-  '18': '0xe9da66965a9344aab2167e6813c03f043cc7a6ca', // DODO v2
-  '19': '0xe592427a0aece92de3edee1f18e0157c05861564', // Uniswap v3
-
-  // The following mappings are best guesses. Replace once mappings have been confirmed.
-  '0x0000000000000000000000000000000442616c616e6365720000000000000000':
-    '0xfe01821ca163844203220cd08e4f2b2fb43ae4e4', // Balancer
-  Bancor: '0xc880c252db7c51f74161633338a3bdafa8e65276', // Bancor
-  '0x0000000000000000000000000000000143757276650000000000000000000000':
-    '0x1796cd592d19e3bcd744fbb025bb61a6d8cb2c09', // Curve
-  Cream: '0xb9d4bf2c8dab828f4ffb656acdb6c2b497d44f25', // Cream
-  Cryptocom: '0x015850307f6aab4ac6631923ceefe71b57492c9b', // Crypto.com
-  DODO: '0xe9da66965a9344aab2167e6813c03f043cc7a6ca', // DODO
-  '0x000000000000000000000000000000054b796265720000000000000000000000':
-    '0xadd97271402590564ddd8ad23cb5317b1fb0fffb', // Kyber
-  '0x000000000000000000000000000000064d6f6f6e697377617000000000000000':
-    '0x02b7eca484ad960fca3f7709e0b2ac81eec3069c', // Mooniswap
-  mStable: '0x2bf04fcea05f0989a14d9afa37aa376baca6b2b3', // mStable
-  Oasis: '0x991c745401d5b5e469b8c3e2cb02c748f08754f1', // Oasis
-  Shell: '0xf1c0811e3788caae7dbfae43da9d9131b1a8a148', // Shell
-  '0x0000000000000000000000000000000253757368695377617000000000000000':
-    '0x47ed0262a0b688dcb836d254c6a2e96b6c48a9f5', // Sushiswap
-  Swerve: '0xf9786d5eb1de47fa56a8f7bb387653c6d410bfee', // Swerve
-  UniswapV1: '0x36691c4f426eb8f42f150ebde43069a31cb080ad', // Uniswap v1
-  '0x00000000000000000000000000000002556e6973776170563200000000000000':
-    '0xdcd6011f4c6b80e470d9487f5871a0cba7c93f48', // Uniswap v2
-  DODOV2: '0xe9da66965a9344aab2167e6813c03f043cc7a6ca', // DODO v2
-  UniswapV3: '0xe592427a0aece92de3edee1f18e0157c05861564', // Uniswap v3
-};
-
 const dedupeEvents = events => {
   return _.uniqWith(events, (a, b) => _.isEqual(a.data, b.data));
-};
-
-/**
- This is a temporary solution until a more robust liquidity source
- attribution engine can be built.
- */
-const mapSourceToBridgeAddress = (source, logger) => {
-  const bridge = SOURCE_BRIDGE_MAPPINGS[source];
-
-  if (bridge === undefined) {
-    logger.warn(`Unrecognised source: ${source}`);
-
-    return undefined;
-  }
-
-  return bridge;
 };
 
 const processTransformedERC20Event = async (
@@ -195,10 +134,6 @@ const processTransformedERC20Event = async (
             amount: new BigNumber(
               bridgeFillEvent.data.inputTokenAmount,
             ).toNumber(),
-            bridgeAddress: mapSourceToBridgeAddress(
-              bridgeFillEvent.data.source,
-              logger,
-            ),
             tokenAddress: bridgeFillEvent.data.inputToken.toLowerCase(),
           },
           {
